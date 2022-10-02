a155352547d6ca6723ec70ee04ae18ad00bb5dfa
ripley yang <ripley.yang@gmail.com>
2022-10-02 22:58:12 +1030

    add documents

---
+++ 35middle-app-develop/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Next.js Boilerplate Presentation"
+          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+        />
+      }
+    >
+      <button className="btn-primary btn">Hello world 35button</button>
+    </Main>
+  );
+};
+
+export default Index;
